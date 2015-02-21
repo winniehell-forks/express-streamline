897922f9e9cbd466047733393f817a0e64b8a506
winniehell <git@winniehell.de>
2015-02-22 00:10:08 +0100

    Add support for Express 4 and 5 Routers
    patch `Layer.handle` instead of `express.application`

--- index.js
+++ index.js
@@ -9,147 +9,44 @@ try {
     streamlineGlobal = require('streamline/lib/globals');
 } catch (err) {}
 
-//
-// Helper function to wrap the given Streamline-style handler (req, res, _)
-// to the style Express requires (req, res, next).
-//
-// Returns the wrapped (req, res, next) handler for Express.
-//
-// Supports other types of handlers too, e.g. with different signatures, via
-// the `verb` param. Non-route verbs include `use` and `param`.
-//
-// Middleware handlers (`use`) have the same signature as route handlers, but
-// unlike route handlers, `next()` is called by default.
-//
-// Param handlers also call `next()` by default, but their signature is
-// (req, res, _, param, key).
-//
-// Both route handlers and middleware handlers can be error handlers, with
-// the signature (err, req, res, _). This is detected by the handler arity.
-//
-// (Express 2 had an explicit `error` verb. That works too.)
-//
-// All handlers can override the default next() behavior by explicitly
-// returning true to call next() or false to not call next().
-//
-function wrap(handler, verb) {
-    var isErrorHandler = (verb === 'error') ||
-        (verb !== 'param' && handler.length >= 4);
+var Layer = require('express/lib/router/layer');
+Layer.prototype.handle_request = function handle(req, res, next) {
+    var fn = this.handle;
 
-    // unify express 2 and 3 to imaginary 'error' verb:
-    if (isErrorHandler) {
-        verb = 'error';
+    if (fn.length > 3) {
+        // not a standard request handler
+        return next();
     }
 
-    var maxArgs = verb === 'param' ? 5 : 4;
-    if (handler.length > maxArgs) {
-        var handlerStr = handler.toString().replace(/(^|\n)/g, '$1| ');
-        console.warn(
-            'Warning: Express app.%s() handler registered with >%d args. ' +
-            'Express-streamline doesn’t know how to handle these; ' +
-            'truncating to %d args.\n\n%s\n',
-            verb, maxArgs, maxArgs, handlerStr
-        );
-    }
+    try {
+        function callback(next) {
+            return function (err) {
+                if (err) return next(err);
 
-    function callback(next) {
-        return function (err, result) {
-            if (err) return next(err);
-
-            var callNext = null;
-
-            // handlers can explicitly return true or false to specify:
-            if (typeof result === 'boolean') {
-                callNext = result;
+                if (err === null) {
+                    next();
+                }
             }
-
-            // otherwise, the default is true for middleware and param
-            // handlers, false for route and error handlers:
-            if (typeof callNext !== 'boolean') {
-                callNext = (verb === 'use') || (verb === 'param');
-            }
-
-            if (callNext) {
-                return next();
-            }
-        };
-    }
-
-    switch (verb) {
-        case 'param':
-            return function (req, res, next, param, key) {
-                return handler.call(this, req, res, callback(next), param, key);
-            }
-        case 'error':
-            return function (err, req, res, next) {
-                return handler.call(this, err, req, res, callback(next));
-            }
-        default:
-            return function (req, res, next) {
-                return handler.call(this, req, res, callback(next));
-            }
-    }
-}
-
-// Express's prototype, with back-compat for Express 2.
-// TODO: This only patches HTTP servers in Express 2, not HTTPS ones.
-var proto = express.application || express.HTTPServer.prototype;
-
-// Checks whether fn is a request handler. Note that Express apps are excluded to avoid patching them below.
-// For more information see https://github.com/aseemk/express-streamline/pull/16.
-function isRequestHandler(fn) {
-    if (typeof fn !== 'function') {
-        return false;
-    }
-
-    // check if fn is an Express app
-    // condition taken from here: https://github.com/strongloop/express/blob/4.11.2/lib/application.js#L186
-    if (fn.handle && fn.set) {
-        return false;
-    }
-
-    return true;
-}
-
-//
-// Helper function to patch proto[verb], to wrap passed-in Streamline-style
-// handlers (req, res, _) to the style Express needs (req, res, next).
-//
-function patch(verb) {
-    var origProtoVerb = proto[verb];
-
-    // minor: don't patch verbs that aren't implemented by Express:
-    if (typeof origProtoVerb !== 'function') {
-        return;
-    }
-
-    proto[verb] = function () {
-        // if a handler function is given, it'll be the last argument:
-        var last = arguments.length - 1;
-        var lastArg = arguments[last];
-
-        // if there is one, wrap it:
-        if (isRequestHandler(lastArg)) {
-            arguments[last] = wrap(lastArg, verb);
         }
 
-        // finally, call the original method now with the updated args:
-        return origProtoVerb.apply(this, arguments);
-    };
-}
+        if(this.route) {
+            fn(req, res, callback(next));
+        }
+        else {
+            fn(req, res, next);
+        }
 
-// Patch all proto methods, e.g. proto.get(), proto.post(), etc.
-// Important: we don't patch `all`, as that's just a simple wrapper.
-require('methods').concat('del', 'error', 'use', 'param')
-    .forEach(function (verb) {
-        patch(verb);
-    });
+    } catch (err) {
+        next(err);
+    }
+};
 
 // Patch app.handle() to reset Streamline's global context at the beginning of
 // every request. This method is only present in Connect's prototype, *not*
 // Express's, so we patch proto.init(), which is called on app creation.
 // https://github.com/strongloop/express/blob/3.15.2/lib/express.js#L39
 if (streamlineGlobal) {
+    var proto = express.application;
     var origProtoInit = proto.init;
 
     proto.init = function () {
